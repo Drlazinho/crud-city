42d0876682a45b8aee2dce178329d20e0192ab81
Drlazinho <lazbonfim@hotmail.com>
2022-08-16 15:50:56 -0300

    feat: Layout Base Responsivo

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,39 +1,45 @@
 import { Icon, IconButton, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { Box } from "@mui/system"
+import { ReactNode } from "react";
 import { useDrawerContext } from "../context";
 
 interface ILayoutBaseDePaginaProps {
   titulo: string;
+  barraDeFerramentas?: ReactNode;
   children: React.ReactNode;
 }
 
 
-export const LayoutBaseDePagina: React.FC <ILayoutBaseDePaginaProps>= ({children, titulo}) => {
+export const LayoutBaseDePagina: React.FC <ILayoutBaseDePaginaProps>= ({children, titulo, barraDeFerramentas}) => {
   const theme = useTheme()
   const smDown = useMediaQuery(theme.breakpoints.down('sm'))
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'))
 
   const {toggleDrawerOpen} = useDrawerContext();
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
-      <Box display="flex" alignItems="center" padding={1} height={theme.spacing(12)} gap={2}>
+      <Box display="flex" alignItems="center" padding={1}  gap={2} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}>
         {smDown && (
         <IconButton onClick={toggleDrawerOpen}>
           <Icon>menu</Icon>
         </IconButton>
-
         )}
 
-        <Typography variant="h5">
+        <Typography variant={smDown ? "h5" : mdDown ? "h4" : "h3"} whiteSpace="nowrap" overflow="hidden" textOverflow="ellipses">
           {titulo}
         </Typography>
       </Box>
       
-      <Box>
-          Barra de Ferramentas
-      </Box>
+      {
+        barraDeFerramentas &&(
+          <Box>
+          {barraDeFerramentas}
+          </Box>
+        )
+      }
 
-      <Box>
+      <Box flex={1} overflow="auto">
         {children}
       </Box>      
     </Box>
