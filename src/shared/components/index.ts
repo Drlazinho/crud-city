885a316cee2760cb82345f155c3bb338a40757cf
Drlazinho <lazbonfim@hotmail.com>
2022-08-17 19:11:24 -0300

    correção

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,3 @@
 export * from './menu-lateral/MenuLateral'
 export * from './ferramentas-da-listagem/FerramentasDaListagem'
-export * from './ferramentas-da-listagem/ferramentas-de-detalhe/FerramentasDeDetalhes'
+export * from './ferramentas-de-detalhe/FerramentasDeDetalhes'
